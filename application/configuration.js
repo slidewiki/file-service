b44046a9293789e8b5e3458115182efd865f2658
Ben <benjamin.wulff.de@ieee.org>
2016-09-21 10:29:30 +0200

    corrected name of env var in configuration script

--- application/configuration.js
+++ application/configuration.js
@@ -3,8 +3,8 @@
 
 const co = require('./common');
 let port = 8080;
-if (!co.isEmpty(process.env.DATABASE_PORT)){
-  port = process.env.DATABASE_PORT;
+if (!co.isEmpty(process.env.APPLICATION_PORT)){
+  port = process.env.APPLICATION_PORT;
 }
 let path = '/data/files';
 if (!co.isEmpty(process.env.APPLICATION_PATH)){
