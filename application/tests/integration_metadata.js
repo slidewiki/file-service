4a169c4f606fd816f7d78debfe4a49e0774497c9
Roy Meissner <meissner@informatik.uni-leipzig.de>
2016-11-15 14:55:29 +0100

    Added some unit and integration tests

---
+++ application/tests/integration_metadata.js
@@ -0,0 +1,102 @@
+'use strict';
+
+describe('REST API', () => {
+
+  let server,db;
+
+  beforeEach((done) => {
+    //Clean everything up before doing new tests
+    Object.keys(require.cache).forEach((key) => delete require.cache[key]);
+    require('chai').should();
+    db = require('../database/mediaDatabase.js');
+    let hapi = require('hapi');
+    server = new hapi.Server();
+    server.connection({
+      host: 'localhost',
+      port: 3000
+    });
+    server.register(require('inert'));
+    require('../routes.js')(server);
+    done();
+  });
+
+  let meta = {
+    title: 'Dummy',
+    type: 'image/jpeg',
+    fileName: 'abc.jpg',
+    thumbnailName: 'abc_thumbnail.jpg',
+    owner: 33,
+    license: 'CC0',
+    copyright: 'Held by no one',
+    metadata: {}
+  };
+
+  let options1 = {
+    method: 'GET',
+    url: '/metadata/abc.jpg',
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  };
+
+  let options2 = {
+    method: 'GET',
+    url: '/metadata/abc_thumbnail.jpg',
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  };
+
+  let options3 = {
+    method: 'GET',
+    url: '/metadata/xyz.jpg',
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  };
+
+  context('when metadata is available', () => {
+    it('should reply it by the filename', (done) => {
+      let tmp = Object.assign({}, meta);
+      db.insert(tmp).then(
+        server.inject(options1, (response) => {() =>
+          response.should.be.an('object').and.contain.keys('statusCode','payload');
+          response.statusCode.should.equal(200);
+          response.payload.should.be.an('string');
+          let payload = JSON.parse(response.payload);
+          payload.should.be.an('object').and.contain.keys(Object.keys(meta));
+          payload.title.should.equal(meta.title);
+          done();
+        })
+      );
+    });
+
+    it('should reply it by the thumbnailname', (done) => {
+      let tmp = Object.assign({}, meta);
+      db.insert(tmp).then(
+        server.inject(options2, (response) => {() =>
+          response.should.be.an('object').and.contain.keys('statusCode','payload');
+          response.statusCode.should.equal(200);
+          response.payload.should.be.an('string');
+          let payload = JSON.parse(response.payload);
+          payload.should.be.an('object').and.contain.keys(Object.keys(meta));
+          payload.title.should.equal(meta.title);
+          done();
+        })
+      );
+    });
+    context('when metadata is not available', () => {
+      it('should reply with 404', (done) => {
+        server.inject(options3, (response) => {() =>
+          response.should.be.an('object').and.contain.keys('statusCode','payload');
+          response.statusCode.should.equal(404);
+          response.payload.should.be.an('string');
+          let payload = JSON.parse(response.payload);
+          payload.should.be.an('object').and.contain.keys('error', 'statusCode');
+          payload.statusCode.should.equal(404);
+          done();
+        });
+      });
+    });
+  });
+});
