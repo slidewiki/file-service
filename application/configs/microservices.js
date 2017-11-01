0d38c609ab8c53e5193cf90f9824f9706fc74bb4
Huw Fryer <huwfryer@gmail.com>
2017-11-01 17:27:34 +0000

    Added platform to config

--- application/configs/microservices.js
+++ application/configs/microservices.js
@@ -3,5 +3,7 @@
 const co = require('../common');
 
 module.exports = {
-  
+  'platform':{
+    uri: (!co.isEmpty(process.env.SERVICE_URL_PLATFORM)) ? process.env.SERVICE_URL_PLATFORM : 'http://platform'
+  },
 };
